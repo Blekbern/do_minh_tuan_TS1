9374da9580ad68ca4019efea3d9fc19fed1c1d73
Tuan <dominhtu@cvut.cz>
2023-03-15 14:00:38 +0100

    idk

---
+++ src/main/java/cz/cvut/fel/ts1/hw02/Calculator.java
@@ -0,0 +1,18 @@
+package cz.cvut.fel.ts1.hw02;
+
+
+public class Calculator {
+
+    public int sum( int a, int b ) {
+        return a + b;
+    }
+    public int subtract( int a, int b ) {
+        return a - b;
+    }
+    public int multiply( int a, int b ) {
+        return a * b;
+    }
+    public int divide( int a, int b ) {
+        return a / b;
+    }
+}
